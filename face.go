164112e8f23361bbed25d071cae07be5d83825dd
Andy Gorman <andyg@apacesystems.com>
2025-01-29 11:25:59 -0800

    Compiling with -lopenblas instead of -lblas -lcblas -llapack

--- face.go
+++ face.go
@@ -1,7 +1,7 @@
 package face
 
 // #cgo CXXFLAGS: -std=c++1z -Wall -O3 -DNDEBUG -march=native
-// #cgo LDFLAGS: -ldlib -lblas -lcblas -llapack -ljpeg
+// #cgo LDFLAGS: -ldlib -lopenblas -ljpeg
 // #include <stdlib.h>
 // #include <stdint.h>
 // #include "facerec.h"
